2c1fe23171931ed0c522e45b6ee5142860ebe2c0
Anericokakai <challykiddoh@gmail.com>
2025-01-05 13:34:44 +0300

    fix

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
+
 export const connectToDataBase=async()=>{
+    console.log(process.env.MONGO)
     const connect=await mongoose.connect(process.env.MONGO)
     return connect
+    
+
 }
